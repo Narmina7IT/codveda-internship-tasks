915df238458004b0164dade5518c67ac5cdce1a8
Narmina <narmina.sharifova.2023@gmail.com>
2026-06-22 18:56:39 +0400

    Add TreeNode class for binary tree structure

---
+++ Level_3/Task_3/TreeNode.java
@@ -0,0 +1,11 @@
+package Level_3.Task_3;
+
+public class TreeNode {
+    int value;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(int value) {
+        this.value = value;
+    }
+}
